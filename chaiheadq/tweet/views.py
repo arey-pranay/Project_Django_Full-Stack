8d31a4cd98fea628138603f3f4bfa936ab8337b7
Pranay Parikh <pranayparikh02@gmail.com>
2025-05-01 17:20:31 +0530

    added conditional rendering of buttons and editinng feature.

--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -8,11 +8,11 @@ from django.contrib.auth import login
 def index(request):
     return render(request, 'index.html')
 
-@login_required  # Ensure the user is logged in to access this view
 def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
+@login_required  # Ensure the user is logged in to access this view
 def tweet_create(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES)
@@ -39,6 +39,7 @@ def tweet_edit(request, tweet_id):
         form = TweetForm(instance=tweet)
     return render(request, 'tweet_form.html', {'form': form})
 
+@login_required  # Ensure the user is logged in to access this view
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user) 
     if request.method == 'POST':
